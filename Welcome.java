eff934c9e92ee07ef783b12df4bd87c1968f8e85
Basha A.A.B.S IT24101503 <it24101503@my.sliit.lk>
2025-07-28 16:27:51 +0530

    Question 01

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Welcome{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your first name: ");
+        String firstName = sc.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastName = sc.nextLine();
+
+	System.out.println("Welcome to the Second Year " + firstName+ " " + lastName);
+
+    }
+}
